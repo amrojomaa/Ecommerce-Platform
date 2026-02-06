597d6ff597dbc3158e3b218502fbfe3c06581f48
Amro Jomaa <jomaamro97@gmail.com>
2026-02-06 14:50:14 +0200

    commit update

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+
+# SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:admin@localhost/fastapi'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)

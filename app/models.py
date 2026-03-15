5f74220e6cd5b4740bad6dccfe96dee79403f607
yazan339 <yoyoyazan22@gmail.com>
2026-03-15 09:27:50 +0200

    Revert "Push local changes"
    This reverts commit 16571a40e1f441e0d6811b1d9898821e801a5e20.

--- app/models.py
+++ app/models.py
@@ -135,7 +135,7 @@ class DBOrderItem(Base):
     __tablename__ = "order_items"
     
     id = Column(Integer, primary_key=True, nullable=False)
-    order_id = Column(Integer, ForeignKey("orders.id"), nullable=False)
+    order_id = Column(Integer, ForeignKey("orders.id", ondelete="CASCADE"), nullable=False)
     product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
     quantity = Column(Integer, nullable=False)
     price = Column(Numeric(10,2), nullable=False)  
